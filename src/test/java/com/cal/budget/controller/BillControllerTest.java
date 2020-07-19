b8bcab4c5833078f150b77975b679ba1c64639ad
Brooke <bsextion@gmail.com>
2020-07-19 15:37:42 -0500

    Unit test added

--- src/test/java/com/cal/budget/controller/BillControllerTest.java
+++ src/test/java/com/cal/budget/controller/BillControllerTest.java
@@ -11,6 +11,8 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -68,7 +70,6 @@ public class BillControllerTest {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
         Date today = formatter.parse(formatter.format(new Date()));
         billList.add(new Bill("Netflix", today, 12.99,"Entertainment"));
-
 
         given(billService.getBillsOnDueDate(today)).willReturn(billList);
 
@@ -128,17 +129,13 @@ public class BillControllerTest {
 
     @Test
     public void shouldcreateBill() throws Exception {
-//        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-//        String id = "45621";
-//        Bill bill = new Bill("Netflix", new Date(), 12.99,"Entertainment");
-//        bill.setId(id);
-//
-//        given(billService.addBill(bill)).t;
-//
-//        this.mockMvc.perform(put("/bill/update", bill.getId())
-//                .contentType(MediaType.APPLICATION_JSON_UTF8)
-//                .content(objectMapper.writeValueAsString(bill)))
-//                .andExpect(status().isOk());
+        given(billService.addBill(any(Bill.class))).willAnswer((invocation -> invocation.getArgument(0)));
+        Bill bill = new Bill("Netflix", new Date(), 12.99,"Entertainment");
+
+        this.mockMvc.perform(post("/bill/add")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(objectMapper.writeValueAsString(bill)))
+                .andExpect(status().isOk());
 
     }
 }
